05781cc9a906fcdb3f983fde8655d4d7576047d3
ankkitsharma <ankitsharma2801@gmail.com>
2025-11-07 19:35:55 +0530

    add shadcnui

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,13 @@
 import styles from "./page.module.css";
 import { prisma } from "@repo/db";
+import {Button} from "@repo/ui/components/button";
 
 export default async function Home() {
     const user = await prisma.user.findFirst()
     return (
         <div className={styles.page}>
             {user?.name ?? "No user added yet"}
+            <Button>Waddup?</Button>
         </div>
     );
 }
