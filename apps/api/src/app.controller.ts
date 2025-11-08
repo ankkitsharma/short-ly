e81c9261543c6c507ad7cc4dcb92b0750a085704
ankkitsharma <ankitsharma2801@gmail.com>
2025-11-08 08:52:41 +0530

    fix database pacakge

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import {prisma} from "@repo/db";
 
 @Controller()
 export class AppController {
@@ -7,6 +8,12 @@ export class AppController {
 
   @Get()
   getHello(): string {
+      const users = prisma.user.findFirst();
+      if (users) {
+          console.log("Users found: ", users);
+      } else {
+          console.log("Users not found");
+      }
     return this.appService.getHello();
   }
 }
