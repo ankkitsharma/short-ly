8f43fb50f119104386d79f8d77515ce1d8266487
ankkitsharma <ankitsharma2801@gmail.com>
2025-11-08 14:45:26 +0530

    add DockerFile for web

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,6 +2,9 @@ import styles from "./page.module.css";
 import { prisma } from "@repo/db";
 import {Button} from "@repo/ui/components/button";
 
+// Force dynamic rendering to avoid prerendering issues during build
+export const dynamic = 'force-dynamic';
+
 export default async function Home() {
     const user = await prisma.user.findFirst()
     return (
