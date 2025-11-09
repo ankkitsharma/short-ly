95153f02bbbba80fa15bfde91639592c4674b93b
ankkitsharma <ankitsharma2801@gmail.com>
2025-11-09 17:39:34 +0530

    initially add auth package

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,23 @@
+import { betterAuth, type BetterAuthOptions } from 'better-auth';
+import { prismaAdapter } from 'better-auth/adapters/prisma';
+import { prisma } from '@repo/db';
+import { config } from 'dotenv';
+
+config();
+
+export const auth = betterAuth<BetterAuthOptions>({
+  database: prismaAdapter(prisma, {
+    provider: 'postgresql',
+  }),
+  trustedOrigins: [process.env.CORS_ORIGIN || ''],
+  emailAndPassword: {
+    enabled: true,
+  },
+  advanced: {
+    defaultCookieAttributes: {
+      sameSite: 'none',
+      secure: true,
+      httpOnly: true,
+    },
+  },
+});
