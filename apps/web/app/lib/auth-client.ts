159dbdafffb9cc7d09f31fb1ba9cfe06a525dd3e
ankkitsharma <ankitsharma2801@gmail.com>
2025-11-19 21:54:43 +0530

    add env attribute for server url

--- apps/web/app/lib/auth-client.ts
+++ apps/web/app/lib/auth-client.ts
@@ -4,7 +4,7 @@ import { auth } from '@repo/auth';
 
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: 'http://localhost:3000',
+  baseURL: process.env.SERVER_URL,
   plugins: [inferAdditionalFields<typeof auth>()],
 });
 
